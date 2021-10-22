d9830b548551769071ca1e0341d85d613d913d88
caesar <x246libra@hotmail.com>
2021-10-22 16:32:46 +0800

    refactor time

--- time_test.go
+++ time_test.go
@@ -1,22 +1,27 @@
 package goutils
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"github.com/Min-Feng/goutils/xLog"
 )
 
 func TestTimeParse(t *testing.T) {
+	xLog.SetGlobalLevel("panic")
 
 	tests := []struct {
 		name      string
 		timeValue string
 	}{
-		{timeValue: "2020-09-04 20:30:30 +0800"},
 		{timeValue: "2020-09-04 20:30:30 -05:30"},
 		{timeValue: "2020-09-04 20:30:30 +04:00"},
-		// {timeValue: "2020-09-04 20:30:30Z"}, // 搞不懂 Z 時區的表示法
+		{timeValue: "2020-10-17"},
+
+		// Z 的用途 和 T 一樣, 標示間隔 同時保有字串連續性
+		// 且同等正負號的位置
+		// {timeValue: "2020-09-04 20:30:30Z"},
 	}
 
 	for _, tt := range tests {
@@ -25,7 +30,7 @@ func TestTimeParse(t *testing.T) {
 			value, err := TimeParse(tt.timeValue)
 			assert.NoError(t, err)
 
-			fmt.Println(value.UTC().Format("2006-01-02 15:04:05 -07:00"))
+			xLog.Debug().Time("unit_test", value).Send()
 		})
 	}
 }
